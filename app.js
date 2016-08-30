5abe868212640a27452a9ebe8f7c70f917f6ad2e
Jiyang Wang <jiyang.wang.us@gmail.com>
2016-08-30 16:34:59 -0700

    multi-user

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes.current_user );
 
 app.get( '/', routes.index );
 app.post( '/create', routes.create );
--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,17 @@ var express = require('express');
 var mongoose = require( 'mongoose' );
 var Todo     = mongoose.model( 'Todo' );
 
-exports.index = function ( req, res ){
-  res.render( 'index', { title : 'Express Todo List' });
-};
 
-exports.create = function ( req, res ){
-  new Todo({
-    content    : req.body.content,
-    updated_at : Date.now()
-  }).save( function( err, todo, count ){
-    res.redirect( '/' );
-  });
-};
-
-// query db for all todo items
-exports.index = function ( req, res ){
+exports.index = function ( req, res, next ){
+  var user_id = req.cookies ?
+    req.cookies.user_id : undefined;
+ 
   Todo.
-    find().
+    find({ user_id : user_id }).
     sort( '-updated_at' ).
     exec( function ( err, todos ){
+      if( err ) return next( err );
+ 
       res.render( 'index', {
           title : 'Express Todo List',
           todos : todos
@@ -28,35 +20,79 @@ exports.index = function ( req, res ){
     });
 };
 
-// remove todo item by its id
-exports.destroy = function ( req, res ){
-  Todo.findById( req.params.id, function ( err, todo ){
-    todo.remove( function ( err, todo ){
-      res.redirect( '/' );
-    });
+exports.create = function ( req, res, next ){
+  new Todo({
+      user_id    : req.cookies.user_id,
+      content    : req.body.content,
+      updated_at : Date.now()
+  }).save( function ( err, todo, count ){
+    if( err ) return next( err );
+ 
+    res.redirect( '/' );
   });
 };
 
-exports.edit = function ( req, res ){
+exports.edit = function( req, res, next ){
+  var user_id = req.cookies ?
+      req.cookies.user_id : undefined;
+ 
   Todo.
-    find().
+    find({ user_id : user_id }).
     sort( '-updated_at' ).
     exec( function ( err, todos ){
+      if( err ) return next( err );
+ 
       res.render( 'edit', {
-          title   : 'Express Todo List Edit',
-          todos   : todos,
-          current : req.params.id
+        title   : 'Express Todo List Edit',
+        todos   : todos,
+        current : req.params.id
       });
     });
 };
 
-// redirect to index when finish
-exports.update = function ( req, res ){
+exports.destroy = function ( req, res, next ){
   Todo.findById( req.params.id, function ( err, todo ){
-    todo.content    = req.body.content;
-    todo.updated_at = Date.now();
-    todo.save( function ( err, todo, count ){
+    var user_id = req.cookies ?
+      req.cookies.user_id : undefined;
+ 
+    if( todo.user_id !== req.cookies.user_id ){
+      return utils.forbidden( res );
+    }
+ 
+    todo.remove( function ( err, todo ){
+      if( err ) return next( err );
+ 
       res.redirect( '/' );
     });
   });
+};
+
+exports.update = function( req, res, next ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    var user_id = req.cookies ?
+      req.cookies.user_id : undefined;
+ 
+    if( todo.user_id !== user_id ){
+      return utils.forbidden( res );
+    }
+ 
+    todo.content    = req.body.content;
+    todo.updated_at = Date.now();
+    todo.save( function ( err, todo, count ){
+      if( err ) return next( err );
+ 
+      res.redirect( '/' );
+    });
+  });
+};
+
+exports.current_user = function ( req, res, next ){
+  var user_id = req.cookies ?
+      req.cookies.user_id : undefined;
+ 
+  if( !user_id ){
+    res.cookie( 'user_id', utils.uid( 32 ));
+  }
+ 
+  next();
 };
